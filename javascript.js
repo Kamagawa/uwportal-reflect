b6dd422f4ecba1ee5574a708a238a751eadc61e4
Jason P'ng <jsjpng@gmail.com>
2016-10-29 13:53:26 -0400

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -1,4 +1,103 @@
 angular.module('portalApp')
+
+.controller('ReflectCtrl', ['$scope', 'ReflectFactory', function ($scope, ReflectFactory) {
+	console.log('ctrl!');
+	
+    // Import variables and functions from service
+    $scope.insertValue = ReflectFactory.insertValue;
+    console.log('hi2');
+    $scope.loading = ReflectFactory.loading;
+    $scope.dbData = ReflectFactory.dbData;
+    
+    // initialize the service
+    ReflectFactory.init($scope);
+console.log('hi');
+    // watch for changes in the loading variable
+    $scope.$watch('loading.value', function () {
+        // if loading
+        if ($scope.loading.value) {
+            // show loading screen in the first column, and don't append it to browser history
+            $scope.portalHelpers.showView('loading.html', 1, false);
+            // show loading animation in place of menu button
+            $scope.portalHelpers.toggleLoading(true);
+        } else {
+			console.log('showing main');
+            $scope.portalHelpers.showView('ReflectMain.html', 1);
+            $scope.portalHelpers.toggleLoading(false);
+        }
+    });
+
+    // Create table, invoked by a button press from database test example
+    $scope.createTable = function () {
+        $scope.portalHelpers.invokeServerFunction('createTable').then(function (result) {
+            $scope.dbData.value = [];
+        });
+    }
+
+    // Handle form submit in the database test example
+    $scope.insertData = function () {
+        if ($scope.insertValue.value.length > 50)
+            alert('value should be less than 50 characters');
+        else {
+            $scope.portalHelpers.invokeServerFunction('insert', {
+                value: $scope.insertValue.value
+            }).then(function (result) {
+                $scope.dbData.value = result;
+            });
+            $scope.insertValue.value = "";
+        }
+    };
+
+}])
+    // Factory maintains the state of the widget
+    .factory('ReflectFactory', [ function () {
+        var initialized = {
+            value: false
+        };
+
+        // Your variable declarations
+        var loading = {
+            value: true
+        };
+        var insertValue = {
+            value: null
+        };
+
+        var dbData = {
+            value: null
+        };
+
+        var sourcesLoaded = 0;
+
+        var init = function ($scope) {
+            if (initialized.value)
+                return;
+            initialized.value = true;
+
+			console.log('getting data.. ', $scope.portalHelpers,  $scope.portalHelpers.invokeServerFunction);
+            // Place your init code here:
+            $scope.portalHelpers.invokeServerFunction('getData').then(function (result) {
+				console.log('got data: ', result);
+                dbData.value = result;
+                sourceLoaded();
+            });
+        }
+
+        function sourceLoaded() {
+            sourcesLoaded++;
+            if (sourcesLoaded > 0)
+                loading.value = false;
+        }
+
+        return {
+            init: init,
+            loading: loading,
+            insertValue: insertValue,
+            dbData: dbData
+        };
+
+    }]);
+/*
 .controller('reflectCtrl', ['$scope', function ($scope) {
 	
 	// mock data
@@ -45,3 +144,5 @@ angular.module('portalApp')
 		$scope.portalHelpers.showView('reflectDetails.html', 2);
 	};
 }]);
+
+*/
